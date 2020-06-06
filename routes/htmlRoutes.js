aefc96f3f67418a7a7d5b5ce7ae1759ca77e7660
Carlos Gasperi <cgasperi90@gmail.com>
2020-06-06 14:44:56 -0400

    creating some html elements and additional pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.render("index")
     });
+
+    app.get("/savedarticles", function(req, res) {
+        res.render("savedarticles")
+    });
 };
